9134c71d68e617100ab0f8ca2fb57a5bb9150090
luciferisangoat-stack <luciferisangoat@gmail.com>
2026-09-18 12:54:35 -0600

    Build installable LuciChat Android app

---
+++ android/app/src/main/java/com/lucichat/app/MainActivity.java
@@ -0,0 +1,39 @@
+package com.lucichat.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private static final String LUCI_URL = "https://lucichat-iesrpp.v2.appdeploy.ai/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(false);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(LUCI_URL);
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = (WebView) findViewById(android.R.id.content);
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
